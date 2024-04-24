d7bfd8fea7da3ebb30befc137d3f7be00a5748c6
unknown <SSDD@PNAWSA06LC1001.duoc.acad>
2024-04-24 16:03:37 -0400

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+
+    let num1= 0;
+    let num2 = 0;
+    let resultado = 0;
+
+
+
+    $("#resultado").css("color","red");
+    $("#resultado").css("font-size","1.2rem");
+    $("#resultado").hide();
+
+    //un selector de etiqueta
+    $("h1").click(function(){
+        $(this).text("¡somos el mejor pais de chile hermano!")
+    });
+
+    $("#btnSumar").click(function(){
+       // e.preventDevault();
+        //console.log("me estoy llamando bien")
+    
+        //$("#error1").css("color,red");
+       //if(num1 == ""){
+        //console.log("hola");
+       //}
+        
+       $("#error1").text("");
+       $("#error2").text("");
+       $("#resultado").text("");
+
+       if($("#numero1").val()==="") {
+
+       
+            $("#error1").text("debe agregar el primer numero");
+            $("#error1").text("color,red");
+            return;
+        }
+
+        if($("#numero2").val()==="") {
+
+       
+           $("#error2").text("debe agregar el primer numero");
+           $("#error2").text("color,red");
+           return;
+        }
+        num1 = pareInt($("numero1").val())
+        num2 = pareInt($("numero1").val())
+        resultado =  num1 + num2;
+        
+        $("#Resultado").show();
+        $("#Resultado").text(resultado);
+        
+        //console.log(num1);
+        //console.log(typeof(num1));
+    });
+
+    $("#btnLimpiar").click(function(){
+        $
+     });
+
+
+    
+
+
+    
+
+});
+
+let resultado = document.getElementById("resultado");
+            resultado.style.displaty = "none";
+            resultado.style.displaty = "block";
+
+
+// let btnSumar = document.getElementById("btnSumar");
+
+// let idchile = document.getElementById("idchile");
+// idchile.addEventListener("click", function(){
+//    idchile.innerHTML = "No tengo porque estar de acuerdo con lo quye pienso";
+// })
